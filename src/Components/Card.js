362cdab3fdc6342ecb01213ea736a171a9ebd0ff
michaelt17 <michaeltamkin2020@u.northwestern.edu>
2019-01-13 14:13:17 -0600

    Added basic Card functionality and a settings button

---
+++ src/Components/Card.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './Card.css'
+
+
+class Card extends Component{
+  render() {
+    let data = {'place': 'Sears Tower', 'tag': 'Tallest building in Chicago',
+                  'distance': '.4 miles'};
+    return (
+      <div id='main_card'>
+        <p>
+          {data['place']}
+        </p>
+        <p>
+          {data['tag']}
+        </p>
+        <p>
+          {data['distance']}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Card;
---
+++ src/Components/Settings.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Settings.css'
+
+class Settings extends Component {
+  render() {
+    return (
+
+      <button>Click</button>
+    );
+  }
+}
+
+export default Settings
