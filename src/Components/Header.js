b2590bc33e89899dc2290e4658590f4467dc6eb0
michaelt17 <michaeltamkin2020@u.northwestern.edu>
2019-01-13 14:26:00 -0600

    Testing to see if changes still work

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import './Header.css'
 
 class Header extends Component {
   render() {
     return (
-      <div>
+      <div id="header_text">
         <p>Follow The Leader</p>
       </div>
     );
