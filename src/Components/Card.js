66db86aeb412b0c2ad5d4d2a198a8ae338dc4707
michaelt17 <michaeltamkin2020@u.northwestern.edu>
2019-01-15 12:22:15 -0600

    added right arrow

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import styled from 'styled-components';
 
 
@@ -6,25 +6,37 @@ class Card extends Component{
   render() {
     let data = this.props.propdata
     return (
-      <CardContainer>
-        <p>
-          {data['place']}
-        </p>
-        <p>
-          {data['tag']}
-        </p>
-        <p>
-          {data['distance']}
-        </p>
-      </CardContainer>
+        <CardContainer>
+        <div style={{display: "inline-block",float:"left"}}>
+          <p>
+            {data['place']}
+            <br/>
+            {data['tag']}
+            <br/>
+            {data['distance']}
+            <br/>
+          </p>
+        </div>
+          <RightArrow> &#187;</RightArrow>
+        </CardContainer>
     );
   }
 }
+
+
 
 const CardContainer = styled.div`
   background-color: lightgray;
   border-radius: 10px;
   margin: 10px 15px;
+  float: left;
+  width: 90%;
+`;
+
+const RightArrow = styled.div`
+  float: right;
+  display: inline-block;
+  z-index: 1;
 `;
 
 export default Card;
