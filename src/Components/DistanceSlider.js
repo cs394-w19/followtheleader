e0e08c250ff353581be8773d8f8dcd93bddac7d7
Jonathan Chan <jonathanchan@u.northwestern.edu>
2019-01-17 01:00:35 -0600

    Fix indentation

--- src/Components/DistanceSlider.js
+++ src/Components/DistanceSlider.js
@@ -5,62 +5,62 @@ import styled from 'styled-components'
 /// The component that lets the user select the distance for places to see.
 /// Pass a prop called handleDistanceChanged as a function to handle the new distance selected.
 export default class DistanceSlider extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            // currentValue is the raw value of the HTML `input` element.
-            // The current distance is computed from this divided by props.numberOfIncrements.
-            currentValue: props.numberOfIncrements / 2
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      // currentValue is the raw value of the HTML `input` element.
+      // The current distance is computed from this divided by props.numberOfIncrements.
+      currentValue: props.numberOfIncrements / 2
     }
+  }
 
-    propTypes: {
-        handleDistanceChanged: PropTypes.func.isRequired,
-        // number of increments on the HTML `input` element.
-        numberOfIncrements: PropTypes.number.isRequired,
-        // The maximum distance that this slider can represent.
-        maxDistance: PropTypes.number.isRequired
-    }
+  propTypes: {
+    handleDistanceChanged: PropTypes.func.isRequired,
+    // number of increments on the HTML `input` element.
+    numberOfIncrements: PropTypes.number.isRequired,
+    // The maximum distance that this slider can represent.
+    maxDistance: PropTypes.number.isRequired
+  }
 
-    handleSlider = event => {
-        const newValue = event.target.value
-        this.setState({
-            currentValue: newValue
-        }, () => {
-            const newDistance = this.currentDistance()
-            this.props.handleDistanceChanged(newDistance)
-        })
-    }
+  handleSlider = event => {
+    const newValue = event.target.value
+    this.setState({
+      currentValue: newValue
+    }, () => {
+      const newDistance = this.currentDistance()
+      this.props.handleDistanceChanged(newDistance)
+    })
+  }
 
-    /// Computes the current distance being represented.
-    currentDistance = () => {
-        return this.state.currentValue / this.props.numberOfIncrements * this.props.maxDistance
-    }
+  /// Computes the current distance being represented.
+  currentDistance = () => {
+    return this.state.currentValue / this.props.numberOfIncrements * this.props.maxDistance
+  }
 
-    render() {
-        return (
-            <Container>
-                <Slider type="range" min={0} max={this.props.numberOfIncrements} value={this.state.currentValue} onChange={this.handleSlider} />
-                <Radius>Radius: {this.currentDistance()} Miles</Radius>
-            </Container>
-        )
-    }
+  render() {
+    return (
+      <Container>
+        <Slider type="range" min={0} max={this.props.numberOfIncrements} value={this.state.currentValue} onChange={this.handleSlider} />
+        <Radius>Radius: {this.currentDistance()} Miles</Radius>
+      </Container>
+    )
+  }
 }
 
 const Container = styled.div`
-    // grid-column: 2;
-    // position: relative;
-    // top: 50%;
-    // transform: translateY(-50%);
-    // width: 100%;
-    text-align: center;
+  // grid-column: 2;
+  // position: relative;
+  // top: 50%;
+  // transform: translateY(-50%);
+  // width: 100%;
+  text-align: center;
 `
 
 const Slider = styled.input`
-    width: 90%;
-    text-align: center;
+  width: 90%;
+  text-align: center;
 `
 
 const Radius = styled.p`
-    margin: 0;
+  margin: 0;
 `
