28d54fb8d6df80d6af24da46cb67aa09688158b6
Jonathan Chan <jonathanchan@u.northwestern.edu>
2019-01-17 10:40:21 -0600

    Use HashRouter

--- src/SiteRoutes.js
+++ src/SiteRoutes.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import App from './App';
 import HomePage from './Components/HomePage';
 import NewPage from './Components/NewPage';
 
 const SiteRoutes = () => (
-  <BrowserRouter onUpdate={() => window.scrollTo(0, 0)}>
+  <HashRouter basename='/followtheleader' onUpdate={() => window.scrollTo(0, 0)}>
     <App>
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/location/:id' component={NewPage}/>
       </Switch>
     </App>
-  </BrowserRouter>
+  </HashRouter>
   );
 
 export default SiteRoutes;
